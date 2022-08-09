76d5b6b95bd3a2ae987a764863c979578ee74c78
Chien Nguyen <nguyenvanchien309318@gmail.com>
2022-08-09 23:03:20 +0700

    update deployment.yaml file

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a46e2760a563240a18dfdba7e817e9f0-1135735559.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
